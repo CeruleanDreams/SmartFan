8b780e22de8527ce3fe33d9d6b4c56716114a408
Nikolai Alexander Verner <113264303+CeruleanDreams@users.noreply.github.com>
2022-11-23 01:40:06 -0500

    Update source.ino
    Used 3.3V algorithm for the TMP35

--- source.ino
+++ source.ino
@@ -1,7 +1,7 @@
 //motor connections
-int enA = 9; //DWM is being applied
-int in1 = 4;
-int in2 = 3;
+int enA = 9; //PWM is set to pin 9
+int in1 = 4; //input 1 is set to pin 4
+int in2 = 3; //input 2 is set to pin 3
 
 int DC_MOTOR_PWM_RATE = 0; 
 int sensorValue;
@@ -23,14 +23,15 @@ void setup()
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sensorValue = analogRead(A0);
-  float voltage = sensorValue * 5.0;
   
+  sensorValue = analogRead(A0); //tmp 36 reads resistance
+  float voltage = sensorValue * 3000;  
   voltage /= 1024.0; 
+  float temp = voltage; // ^ Formula used to transform input voltage into temperature in Celsius
   
-  float temp = (voltage - 0.5) * 40 ; //Used to be * 100, but the STM32 seems to be operating at a higher voltage
+ // float temp = (voltage - 0.5) * 40 ; // Used to be (x100), but the STM32 seems to be operating at a higher voltage compared to the Arduino UNO
 
-  if ((temp > 20) && (temp < 25.00))
+  if ((temp > 20) && (temp < 25.00)) //lowest setting to allow for the fan's operation
   {
     
     digitalWrite(in1, LOW);
@@ -39,7 +40,7 @@ void loop() {
     analogWrite(enA, 10);
    
   }
-  else if ((temp => 25.00) && (temp < 30.00))
+  else if ((temp > 25.00) && (temp < 30.00)) //middle setting
   {
    
     digitalWrite(in1, LOW);
@@ -48,20 +49,19 @@ void loop() {
    analogWrite(enA, 100);
   }
 
-    else if ((temp => 30.00) && (temp < 37.00))
+    else if ((temp > 30.00) && (temp < 37.00)) //once it reaches body temperature, the fan shuts down
   {
    
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
-    Serial.println("--ON 2-- ");
-   analogWrite(enA, 250);
+    Serial.println("--ON 3-- ");
+   analogWrite(enA, 250); //analogWrite: using PWM to change the output and thus reduce the fan's RPM
   }
   
   else {
-
-    delay (50);
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);  
+    delay (100); //small delay in case of sudden temperature spikes or glitches
+    digitalWrite(in1, HIGH); //fan spins in one direction only
+    digitalWrite(in2, LOW);  //switching the direction makes the fan halt
   }
 
   
@@ -69,15 +69,3 @@ void loop() {
   delay(5);
 
 }
-
-/* void on(int s){// 0 <= s <256
-  analogWrite(enA,s);
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);  
-  }
-
-void off(){
-  digitalWrite(in1, LOW);
-   digitalWrite(in2, LOW);  
-  
-  } */
